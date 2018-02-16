b8559c57e47dcb5145737e4afa1b31fe5dbcc481
Samuel Sharp <league@WTS13.local>
2018-02-16 14:42:09 -0800

    Day 1

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -6,13 +6,21 @@ import org.jointheleague.graphical.robot.Robot;
 public class FourSquare {
 	
 	// 2. Create a new Robot
+	Robot Bob = new Robot("batman");
 
 	void go() {
 		// 4. Make the robot move as fast as possible
+		Bob.setSpeed(99999);
 
 		// 5. Set the pen width to 5
+		Bob.setPenWidth(5);
 
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
+		for (int i=0;i<4;i++) {
+			Bob.setRandomPenColor();
+			drawSquare();
+			Bob.turn(90);
+					}
 
 			// 7. Set the pen color to random
 	
@@ -24,8 +32,16 @@ public class FourSquare {
 
 	/* 3. Fill in the code to draw a square inside the method below. */
 	void drawSquare() {
-		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
-
+		JOptionPane.showMessageDialog(null, "YOU JUST DREW A SQUARE! HOW COULD YOU?!?!?");
+		Bob.penDown();
+		Bob.move(100);
+		Bob.turn(90);
+		Bob.move(100);
+		Bob.turn(90);
+		Bob.move(100);
+		Bob.turn(90);
+		Bob.move(100);
+		Bob.turn(90);
 		
 		
 	}
--- src/section2/Spiral.java
+++ src/section2/Spiral.java
@@ -5,19 +5,29 @@ import org.jointheleague.graphical.robot.Robot;
 public class Spiral {
 
 	public static void main(String[] args) {
-		// 1. Create a new Robot
+		Robot Bob = new Robot("batman");
 		
-		// 5. Set your robot's pen down 
+		Bob.penDown();
+		
 		
 		// 3. Set the robot to go at max speed (10)
+		Bob.setSpeed(10);
 		
 		// 4. Use a for loop to repeat steps #7 to #8, 75 times
+		for(int i=0;i<75;i++) {
+			Bob.setRandomPenColor();
+			Bob.turn(360/7);
+			Bob.setPenWidth(i);
+			Bob.move(5*i);
+		}
 		
 			// 7. Change the pen color to random
 		
 			// 6. Move the robot 5 times the current line number you are drawing (5*i)
 			
 			// 2. Turn the robot 360/7 degrees to the right
+		Bob.turn(360/7);
+		
 		
 			// 8. Set the pen width to i
 			
